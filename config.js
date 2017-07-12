f3ff855b7d345e18339486c0288d16ec5b93729f
Kendallyn <Kendallynlyon@hotmail.com>
2017-07-12 12:04:03 -0700

    initial structure complete

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL ||
     (process.env.NODE_ENV === 'production' ?
-     'mongodb://admin:password@ds143181.mlab.com:43181/active-api-hiking-search' :
-     'mongodb://admin:password@ds143181.mlab.com:43181/active-api-hiking-search');
+     'mongodb://admin:password@ds155192.mlab.com:55192/national-parks-final-capstone' :
+     'mongodb://admin:password@ds155192.mlab.com:55192/national-parks-final-capstone');
 exports.PORT = process.env.PORT || 5002;
--- public/app.js
+++ public/app.js
@@ -15,14 +15,14 @@
 
 
 
-//User will be able to add a location to a 'Want to visit' section
+//User will be able to add a location to 'National Park Bucket List' section
 
 
 
 
-//User will be able to add a location to a 'Loved my visit' section
+//User will be able to 'check' item as a place visited
 
 
 
 
-//User will be able to delete a saved place from either section
+//User will be able to remove item from list
