36128c0afd5260d955050dda01dbfaacea268dff
Kendallyn <Kendallynlyon@hotmail.com>
2017-08-17 10:32:37 -0700

    added React test

--- test/test-nps-api-national-parks.js
+++ test/test-nps-api-national-parks.js
@@ -6,7 +6,7 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 //import React from 'react';
-import {shallow} from 'enzyme';
+//import {shallow} from 'enzyme';
 import Intro from './components/intro';
 import HowItWorks from './components/howitworks';
 import Instructions from './components/instructions';
